e45f3cb1e4b132829adb0a44e857543c0fc190b3
Nate Koike <nkoike1998@gmail.com>
2020-11-27 23:47:40 -1000

    mostly getting pages

--- scrape.py
+++ scrape.py
@@ -1,5 +1,30 @@
 import urllib.request
+from fpdf import FPDF
+import cv2
+import numpy
 
 i = 1
 
-urllib.request.urlretrieve(f"https://content.wdl.org/10096/service/thumbnail/1431369762/1024x1024/1/{i}.jpg", "img.jpg")
+# create a new pdf
+pdf = FPDF()
+
+# until theres an error
+while True:
+    if i > 10: break
+    try:
+        pdf.add_page()
+
+        fname = f"img{i}.jpg"
+        url = f"https://content.wdl.org/10096/service/thumbnail/1431369762/1024x1024/1/{i}.jpg"
+
+        urllib.request.urlretrieve(url, fname)
+
+        pdf.image(fname, type="jpg")
+
+        i += 1
+
+    except:
+        break
+
+# write to the pdf
+pdf.output("florentine.pdf")
