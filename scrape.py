859c26811aeccd8692ddc5627e754215d086896b
Nate Koike <nkoike1998@gmail.com>
2020-11-28 00:05:17 -1000

    added git ignore and finished web scraper

--- scrape.py
+++ scrape.py
@@ -3,26 +3,33 @@ from fpdf import FPDF
 import cv2
 import numpy
 
+# current page number (not an index)
 i = 1
 
 # create a new pdf
-pdf = FPDF()
+pdf = FPDF('P', "pt", (800, 1024))
 
 # until theres an error
 while True:
-    if i > 10: break
+    # until theres an error
     try:
-        pdf.add_page()
+        # add a new page in the pdf
+        pdf.add_page(orientation='P')
 
+        # set the filename and the url
         fname = f"img{i}.jpg"
         url = f"https://content.wdl.org/10096/service/thumbnail/1431369762/1024x1024/1/{i}.jpg"
 
+        # fetch the image from the url and save it so we can add it to the pdf
         urllib.request.urlretrieve(url, fname)
 
-        pdf.image(fname, type="jpg")
+        # add the image to the pdf
+        pdf.image(fname, type="jpg", x=0, y=0)
 
+        # move on to the next image
         i += 1
 
+    # if theres an error er can end
     except:
         break
 
